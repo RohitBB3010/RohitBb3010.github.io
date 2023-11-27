e7bfc6d2f896bfbfb929440d712ec3d912e4eda2
Rohit Bharat Bhandwalkar <rohitb.bhandwalkar@gmail.com>
2023-11-27 12:03:35 +0530

    Added text to experience text

--- lib/Strings/experience_txt.dart
+++ lib/Strings/experience_txt.dart
@@ -1,2 +1,2 @@
 const String internshipInfo =
-    'Developed custom applications using Flutter & Firebase for clients. Active participation in team meetings and updates, fosteringteamwork and growth.Documented decisions for app scalability';
+    'Developed custom applications using Flutter & Firebase for clients. Active participation in team meetings and updates, fosteringteamwork and growth.Documented decisions for app scalability, ensuring adaptability to changing needs.';
